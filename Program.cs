91af4258cabdc221576d1319e55f8ec6a4fa033a
Jan Christian Becker <s.ecu@hotmail.de>
2024-03-07 15:47:20 +0100

    Update Style
    Many Changes

--- Program.cs
+++ Program.cs
@@ -17,22 +17,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddHsts(options =>
-{
-    options.Preload = true;
-    options.IncludeSubDomains = true;
-    options.MaxAge = TimeSpan.FromDays(60);
-    options.ExcludedHosts.Add("example.com");
-    options.ExcludedHosts.Add("www.example.com");
-});
-
-builder.Services.AddHttpsRedirection(options =>
-{
-    int Status307TemporaryRedirect = 0;
-    options.RedirectStatusCode = Status307TemporaryRedirect;
-    options.HttpsPort = 5001;
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -59,7 +43,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=LandingPage}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
