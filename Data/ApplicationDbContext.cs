13bdb3d9cbbec0cc5db815dea87b3a40ab15aed7
Jan Christian Becker <s.ecu@hotmail.de>
2024-03-10 21:53:28 +0100

    New Features
    Work progress !
    
    Co-Authored-By: Paul Rieser <161761197+Asario2@users.noreply.github.com>

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using cApNetDemoPage.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace cApNetDemoPage.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Members> Members { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Models/Members.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cApNetDemoPage.Models
+{
+    public class Members
+    {
+        public int Id { get; set; }
+
+        public string Prename { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+        
+        public string Birthdate { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Function { get; set; }
+    }
+}
