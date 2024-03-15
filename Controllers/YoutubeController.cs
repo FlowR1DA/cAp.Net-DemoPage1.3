81e625272b7d9cec6baeae67f54062cd937bf5b6
Jan Christian Becker <s.ecu@hotmail.de>
2024-03-15 23:14:55 +0100

    New 15th March 24
    Update

---
+++ Controllers/YoutubeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cApNetDemoPage.Controllers
+{
+    public class YoutubeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
